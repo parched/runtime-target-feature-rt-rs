f30766b6dd36867eeb0a58bf938d65e13a5862b6
James Duley <jagduley@gmail.com>
2017-06-05 22:57:40 +0100

    Dummy implementations

---
+++ src/x86.rs
@@ -0,0 +1,70 @@
+// TODO: implement all using CPUID
+
+pub fn have_avx() -> bool {
+    true
+}
+
+pub fn have_avx2() -> bool {
+    true
+}
+
+pub fn have_bmi() -> bool {
+    true
+}
+
+pub fn have_bmi2() -> bool {
+    true
+}
+
+pub fn have_sse() -> bool {
+    true
+}
+
+pub fn have_sse2() -> bool {
+    true
+}
+
+pub fn have_sse3() -> bool {
+    true
+}
+
+pub fn have_sse4_1() -> bool {
+    true
+}
+
+pub fn have_sse4_2() -> bool {
+    true
+}
+
+pub fn have_ssse3() -> bool {
+    true
+}
+
+pub fn have_tbm() -> bool {
+    true
+}
+
+pub fn have_lzcnt() -> bool {
+    true
+}
+
+pub fn have_popcnt() -> bool {
+    true
+}
+
+pub fn have_sse4a() -> bool {
+    true
+}
+
+pub fn have_rdrnd() -> bool {
+    true
+}
+
+pub fn have_rdseed() -> bool {
+    true
+}
+
+pub fn have_fma() -> bool {
+    true
+}
+
